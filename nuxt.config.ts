3244018258709a9681373a83654f0d260d89259a
Wayfinder Technologies <117512582+WayfinderTech@users.noreply.github.com>
2024-08-22 11:37:37 -0700

    UI-Pro added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  extends: ['@nuxt/ui-pro'],
   modules: [
     '@nuxt/eslint',
     "@nuxt/ui",
